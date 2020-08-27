52e274ff25f1921015fd7b965966e235632f5daf
Tomasz Janczuk <tomasz@janczuk.org>
2020-08-26 21:51:38 -0700

    Updates

---
+++ precache-manifest.5c46afe9ec560e70adff31fd7d773b94.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041771b4ebeffe8a104efe7f21c53d9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10202bca46bcc8aa93de",
+    "url": "/static/css/main.b73bf190.chunk.css"
+  },
+  {
+    "revision": "198d714137c27dee5725",
+    "url": "/static/js/2.b8ec5416.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b8ec5416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10202bca46bcc8aa93de",
+    "url": "/static/js/main.dbc2d403.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/static/js/main.dbc2d403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3fdab3f86a3b0f2aa6",
+    "url": "/static/js/runtime-main.f693082c.js"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
